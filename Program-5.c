c5c4e1795f5a7ee3297938bf690edf86cd76ee0d
p1234567717 <20ME0018@vvce.ac.in>
2021-03-16 16:32:48 +0000

    S

--- Program-5.c
+++ Program-5.c
@@ -16,7 +16,13 @@ int main()
   printf("Input a string: ");
   scanf("%s", s);
 
-  while (s[c] != '\0') {
+  for(c=0;c<500;c++)
+    {
+      if(s[c]=='a'||s[c]=='A'||s[c   ]=='e'||s[c]=='E'||s[c]=='i'||s[c]=='I'||s[c]=='o'||s[c]=='O'||s[c]=='u'||s[c]=='U')
+      {
+      count++;
+    }
+    
                                //place logic to find the vowel
   }
 
